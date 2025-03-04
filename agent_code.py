98fcbd553a7d17cb2160261fa06369f4acba457c
Eugene Che <36330890+Hardbow@users.noreply.github.com>
2025-03-04 18:00:10 +0300

    added function suggest_menu into agent_code.py

--- agent_code.py
+++ agent_code.py
@@ -2,9 +2,31 @@ import os
 from dotenv import load_dotenv
 
 from huggingface_hub import notebook_login
-from smolagents import CodeAgent, DuckDuckGoSearchTool, HfApiModel
+from smolagents import CodeAgent, tool, DuckDuckGoSearchTool, HfApiModel
 
 load_dotenv()
+
+@tool
+def suggest_menu(occasion: str) -> str:
+    """
+    Suggests a menu base on the occasion.
+    Args:
+        occasion: The type of occasion for the party.
+    """
+    if occasion == "casual":
+        return "Pizza, snacks, and drinks."
+    elif occasion == "formal":
+        return "3-course dinner with wine and dessert."
+    elif occasion == "superhero":
+        return "Buffet with high-energy and healthy food."
+    else:
+        return "Custom menu for the butler."
+
+
+
+
+
+
 
 # os.environ["HF_TOKEN"] = dotenv.
 agent = CodeAgent(tools=[DuckDuckGoSearchTool()], model=HfApiModel())
