d637cbaed78fef1625952ec7b13673630474f359
Eugene Che <36330890+Hardbow@users.noreply.github.com>
2025-03-14 17:40:42 +0300

    changed basic_try.py

--- basic_try.py
+++ basic_try.py
@@ -4,40 +4,36 @@ import datetime
 from dotenv import load_dotenv
 
 from huggingface_hub import login
-from smolagents import CodeAgent, DuckDuckGoSearchTool, HfApiModel, tool
+from smolagents import CodeAgent, DuckDuckGoSearchTool, FinalAnswerTool, HfApiModel, Tool, tool, VisitWebpageTool
+import os
+import sys
+
+sys.stdout.reconfigure(encoding='utf-8')
+sys.stderr.reconfigure(encoding='utf-8')
+
+os.environ["PYTHONIOENCODING"] = 'utf-8'
 
 
-@tool
-def suggest_menu(occasion: str) -> str:
-    """
-    Suggests a menu based on the occasion.
-    Args:
-        occasion: The type of occasion for the party (casual, formal or superhero)
-    """
-    if occasion == "casual":
-        return "Pizza, snacks, and drinks."
-    elif occasion == 'formal':
-        return "3-course dinner with wine and dessert"
-    elif occasion == 'superhero':
-        return "Buffet with high-energy and healthy food."
-    else:
-        return "Custom menu for the butler."
+
 
 
 load_dotenv()
 
-agent = CodeAgent(tools=[], model=HfApiModel(), additional_authorized_imports=['datatime'])
+# agent = CodeAgent(tools=[], model=HfApiModel(), additional_authorized_imports=['datatime'])
 # agent = CodeAgent(tools=[suggest_menu], model=HfApiModel())
 # agent.run("Prepare a formal menu for the party")
-# agent.run("Search for the top 5 best movie of New wave")
-agent.run(
-    """"
-    Alfred needs to prepare for the party. Here are the tasks:
-    1. Prepare the drinks - 30 minutes
-    2. Decorate the mansion - 60 minutes
-    3. Set up the menu - 45 minutes
-    4. Prepare the music and playlist - 45 minutes
+agent = CodeAgent(tools=[DuckDuckGoSearchTool()], model=HfApiModel())
+agent.run("Search for the top 5 best movie of New wave")
+# agent.run(
+#     """"
+#     Alfred needs to prepare for the party. Here are the tasks:
+#     1. Prepare the drinks - 30 minutes
+#     2. Decorate the mansion - 60 minutes
+#     3. Set up the menu - 45 minutes
+#     4. Prepare the music and playlist - 45 minutes
+#
+#     If we start right now, at what time will the party be ready?
+#     """
+# )
 
-    If we start right now, at what time will the party be ready?
-    """
-)
+agent.push_to_hub('YouGene/AlfredAgent')
